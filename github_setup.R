33140d3fd19290a94bca5dec9e2b408e442bba01
jameshading <wezizwe.harding@gmail.com>
2026-03-22 23:55:03 -0500

    initial commit

---
+++ github_setup.R
@@ -0,0 +1,17 @@
+---
+title: "github_setup.R"
+format: html
+editor: visual
+---
+
+#GitHub setup
+#first, sign up for an account at www.github.com
+
+library(usethis)
+
+# connect Rstudio to your
+
+create_github_token()
+
+gitcreds::gitcreds_set()
+
